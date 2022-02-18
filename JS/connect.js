68469eae168af4c4b64b983ab081806fcddf9dd9
ahmedonour <ahmedonour.49@gmail.com>
2022-02-18 17:15:24 +0200

    new

--- JS/connect.js
+++ JS/connect.js
@@ -7,15 +7,15 @@ let db = new sqlite3.Database('../DB/main.db', sqlite3.OPEN_READWRITE, (err) => 
   }
   console.log('Connected to the chinook database.');
 });
-
 db.serialize(() => {
   db.each(`SELECT numper as id,
-                  name as name
+                  name as name,
+                  password as password
            FROM admins`, (err, row) => {
     if (err) {
       console.error(err.message);
     }
-    console.log(row.id + "\t" + row.name);
+    console.log(row.id + "\t" + row.name + "\t" + row.password);
   });
 });
 
